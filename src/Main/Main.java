d5ad18443c1ac172a963f72fbc1675a627a01735
Tao1925 <yhxxtsy@163.com>
2021-06-07 07:24:22 +0800

    Initial commit

---
+++ src/Main/Main.java
@@ -0,0 +1,9 @@
+package Main;
+
+import UI.MainUI;
+
+public class Main {
+    public static void main(String[] args) {
+        MainUI mainUI = new MainUI();
+    }
+}
---
+++ src/UI/MainUI.java
@@ -0,0 +1,18 @@
+package UI;
+
+import javax.swing.*;
+
+public class MainUI extends JFrame{
+
+    public MainUI(){
+        init();
+        setVisible(true);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        validate();
+    }
+
+    public void init(){
+
+    }
+}
